8f3bd3a60315e4b086cb8fbdc0e3b539ca7f7dd2
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2023-11-19 17:54:37 -0300

    Update recon-tools.sh

--- recon-tools.sh
+++ recon-tools.sh
@@ -18,4 +18,9 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
 
 git clone https://github.com/aboul3la/Sublist3r.git ~/tools/sublist3r
-sudo apt install jq
+sudo apt install jq zip unzip
+
+wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip && unzip SecList.zip && rm -f SecList.zip
+mv SecLists-master seclists
+sudo mv -f seclists /usr/share
+
