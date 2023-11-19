36a97e677c8f048475edc415c4786ad551b96bc8
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2023-11-19 17:25:31 -0300

    Create recon-tools.sh

---
+++ recon-tools.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Install Go before run this script
+
+mkdir -p ~/tools
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/owasp-amass/amass/v4/...@master
+go install github.com/tomnomnom/assetfinder@latest
+
+
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+
+
+git clone https://github.com/aboul3la/Sublist3r.git ~/tools/sublist3r
+sudo apt install jq
